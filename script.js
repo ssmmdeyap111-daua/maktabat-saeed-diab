2052066855f74028081088b1d6ca9902ace5e8c9
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-01-29 10:04:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ function moveBooks(dir) {
   const container = document.getElementById("booksContainer");
   container.scrollLeft += dir * 220;
 }
+<button class="arrow" onclick="scrollBooks(-1)">❮</button>
+<button class="arrow" onclick="scrollBooks(1)">❯</button>
+
+<script>
+function scrollBooks(dir) {
+  const c = document.getElementById("booksContainer");
+  c.scrollLeft += dir * 200; // كل مرة تتحرك 200 بكسل
+}
+</script>
 
