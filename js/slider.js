5434595ba7f4032c15eecc45578ce16b90f33c7a
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-04 21:53:46 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,17 @@
 
+const slider = document.querySelector('.books-slider');
+const prev = document.querySelector('.book-prev');
+const next = document.querySelector('.book-next');
+
+let scrollAmount = 0;
+const slideWidth = 160; // عرض الكتاب + المارجن
+
+prev.addEventListener('click', () => {
+    scrollAmount = Math.max(scrollAmount - slideWidth, 0);
+    slider.style.transform = `translateX(-${scrollAmount}px)`;
+});
+
+next.addEventListener('click', () => {
+    scrollAmount += slideWidth;
+    slider.style.transform = `translateX(-${scrollAmount}px)`;
+});
