b28c6afd814d68c2caab863ab100dfac46100d25
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-04 22:12:12 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -15,3 +15,19 @@ next.addEventListener('click', () => {
     scrollAmount += slideWidth;
     slider.style.transform = `translateX(-${scrollAmount}px)`;
 });
+const slider = document.querySelector('.books-slider');
+const prev = document.querySelector('.book-prev');
+const next = document.querySelector('.book-next');
+
+let scrollAmount = 0;
+const slideWidth = 170;
+
+next.addEventListener('click', () => {
+    scrollAmount += slideWidth;
+    slider.style.transform = `translateX(${scrollAmount}px)`;
+});
+
+prev.addEventListener('click', () => {
+    scrollAmount -= slideWidth;
+    slider.style.transform = `translateX(${scrollAmount}px)`;
+});
