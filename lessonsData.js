92014269232e07bf1267fd3ce3c5c004b94fd4aa
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-06 21:52:42 +0300

    Update lessonsData.js

--- lessonsData.js
+++ lessonsData.js
@@ -1,1 +1,24 @@
+// هذا الملف يضم كل الدروس الصوتية
+const lessonsData = {
+    "التفسير": [
+        "tafseer-fatiha-1.mp3",
+        "tafseer-baqara-1.mp3",
+        "tafseer-al-imran-1.mp3"
+    ],
+    "شروح الحديث": [
+        "hadith1.mp3"
+    ],
+    "التوحيد": [
+        "tawheed1.mp3"
+    ],
+    "الفقه": [
+        "fiqh1.mp3"
+    ],
+    "السيرة": [
+        "seerah1.mp3"
+    ],
+    "الرقائق": [
+        "raqaiq1.mp3"
+    ]
+};
 
