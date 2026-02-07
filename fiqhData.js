a75a86e8cd13267021feae88c548e54bf5529464
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-07 21:31:43 +0300

    Update fiqhData.js

--- fiqhData.js
+++ fiqhData.js
@@ -20,9 +20,25 @@ duration: "18 دقيقة"
 title: "شرح المنظومة الرحبية - الدرس الثالث",
 file: "audio/al-rhapih3.mp3",
 book: "الوارثون من الرجال",
-lessonNumber: 2,
+lessonNumber: 3,
 duration: "18 دقيقة"
-}
+},
 
+ {
+title: "شرح المنظومة الرحبية - الدرس الرابع",
+file: "audio/al-rhapih4.mp3",
+book: "الوارثون من الرجال",
+lessonNumber: 4,
+duration: "18 دقيقة"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الخامس",
+file: "audio/al-rhapih5.mp3",
+book: "الوارثون من الرجال",
+lessonNumber: 5,
+duration: "18 دقيقة"
+},
+  
 ];
 
