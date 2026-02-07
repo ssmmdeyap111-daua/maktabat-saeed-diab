79e210adb7db08516ffa91ecf4bf4279461fb178
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-07 21:12:49 +0300

    Update fiqhData.js

--- fiqhData.js
+++ fiqhData.js
@@ -1,28 +1,28 @@
 const fiqhLessons = [
 
 {
-title: "شرح كتاب الطهارة - الدرس الأول",
-file: "tahara-1.mp3",
-book: "كتاب الطهارة",
+title: "شرح المنظومة الرحبية - الدرس الأول",
+file: "audio/al-rhapih1.mp3",
+book: "المقدمة",
 lessonNumber: 1,
 duration: "20 دقيقة"
 },
 
 {
-title: "شرح كتاب الطهارة - الدرس الثاني",
-file: "tahara-2.mp3",
-book: "كتاب الطهارة",
+title: "شرح المنظومة الرحبية - الدرس الثاني",
+file: "audio/al-rhapih2.mp3",
+book: "الوارثون من الرجال",
 lessonNumber: 2,
 duration: "18 دقيقة"
 },
 
 {
-title: "شرح كتاب الصلاة - الدرس الأول",
-file: "salah-1.mp3",
-book: "كتاب الصلاة",
-lessonNumber: 1,
-duration: "25 دقيقة"
-}
+title: "شرح المنظومة الرحبية - الدرس الثالث",
+file: "audio/al-rhapih3.mp3",
+book: "الوارثون من الرجال",
+lessonNumber: 2,
+duration: "18 دقيقة"
+},
 
 ];
 
