67554d5b253ec874a7ac2d18b9a870467dedae5a
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-08 00:48:29 +0300

    Update search-data.js

--- search-data.js
+++ search-data.js
@@ -1,1 +1,28 @@
 
+const siteData = [
+
+{
+title: "تفسير سورة البقرة - الدرس الأول",
+link: "tafseer.html",
+category: "التفسير"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الأول",
+link: "fiqh.html",
+category: "الفرائض"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الثاني",
+link: "fiqh.html",
+category: "الفرائض"
+},
+
+{
+title: "الكتب والمؤلفات",
+link: "books.html",
+category: "كتب"
+}
+
+];
