609dd2a2099d26a01732fe24e249f75a14db9067
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-07 21:05:12 +0300

    Update fiqhData.js

--- fiqhData.js
+++ fiqhData.js
@@ -1,1 +1,28 @@
+const fiqhLessons = [
+
+{
+title: "شرح كتاب الطهارة - الدرس الأول",
+file: "tahara-1.mp3",
+book: "كتاب الطهارة",
+lessonNumber: 1,
+duration: "20 دقيقة"
+},
+
+{
+title: "شرح كتاب الطهارة - الدرس الثاني",
+file: "tahara-2.mp3",
+book: "كتاب الطهارة",
+lessonNumber: 2,
+duration: "18 دقيقة"
+},
+
+{
+title: "شرح كتاب الصلاة - الدرس الأول",
+file: "salah-1.mp3",
+book: "كتاب الصلاة",
+lessonNumber: 1,
+duration: "25 دقيقة"
+}
+
+];
 
