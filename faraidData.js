ce67f656495af529484dbdd131ec8e2159d5313c
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-08 00:25:28 +0300

    Update faraidData.js

--- faraidData.js
+++ faraidData.js
@@ -1,1 +1,39 @@
 
+const faraidLessons = [
+
+{
+title: "شرح المنظومة الرحبية - الدرس الأول",
+file: "audio/al-rhapih1.mp3",
+lessonNumber: 1,
+duration: "20 دقيقة"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الثاني",
+file: "audio/al-rhapih2.mp3",
+lessonNumber: 2,
+duration: "18 دقيقة"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الثالث",
+file: "audio/al-rhapih3.mp3",
+lessonNumber: 3,
+duration: "18 دقيقة"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الرابع",
+file: "audio/al-rhapih4.mp3",
+lessonNumber: 4,
+duration: "18 دقيقة"
+},
+
+{
+title: "شرح المنظومة الرحبية - الدرس الخامس",
+file: "audio/al-rhapih5.mp3",
+lessonNumber: 5,
+duration: "18 دقيقة"
+}
+
+];
