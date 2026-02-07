a52ac233cd273e96fdf99bf8a09faa3d38781b49
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-07 21:47:12 +0300

    Update fiqhData.js

--- fiqhData.js
+++ fiqhData.js
@@ -2,7 +2,7 @@ const fiqhLessons = [
 
 {
 title: "شرح المنظومة الرحبية - الدرس الأول",
-file: "audio/al-rhapih1.mp3",
+file: "al-rhapih1.mp3",
 book: "المقدمة",
 lessonNumber: 1,
 duration: "20 دقيقة"
@@ -10,7 +10,7 @@ duration: "20 دقيقة"
 
 {
 title: "شرح المنظومة الرحبية - الدرس الثاني",
-file: "audio/al-rhapih2.mp3",
+file: "al-rhapih2.mp3",
 book: "موانع الإرث",
 lessonNumber: 2,
 duration: "18 دقيقة"
@@ -18,15 +18,15 @@ duration: "18 دقيقة"
 
 {
 title: "شرح المنظومة الرحبية - الدرس الثالث",
-file: "audio/al-rhapih3.mp3",
+file: "al-rhapih3.mp3",
 book: "باب من يرث الربع",
 lessonNumber: 3,
 duration: "18 دقيقة"
 },
 
- {
+{
 title: "شرح المنظومة الرحبية - الدرس الرابع",
-file: "audio/al-rhapih4.mp3",
+file: "al-rhapih4.mp3",
 book: "باب من يرث السدس",
 lessonNumber: 4,
 duration: "18 دقيقة"
@@ -34,11 +34,10 @@ duration: "18 دقيقة"
 
 {
 title: "شرح المنظومة الرحبية - الدرس الخامس",
-file: "audio/al-rhapih5.mp3",
+file: "al-rhapih5.mp3",
 book: "ميراث الجدات",
 lessonNumber: 5,
 duration: "18 دقيقة"
-},
-  
-];
+}
 
+];
