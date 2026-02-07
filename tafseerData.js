9323c5d7060b7feb23e8c5d5644f42665cca56e0
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-07 17:21:49 +0300

    Update tafseerData.js

--- tafseerData.js
+++ tafseerData.js
@@ -2,32 +2,38 @@ const tafseerLessons = [
 
 {
 title: "تفسير سورة الفاتحة - الدرس الأول",
-file: "fatiha-1.mp3"
+file: "fatiha-1.mp3",
+duration: "15 دقيقة"
 },
 
 {
 title: "تفسير سورة الفاتحة - الدرس الثاني",
-file: "fatiha-2.mp3"
+file: "fatiha-2.mp3",
+duration: "18 دقيقة"
 },
 
 {
 title: "تفسير سورة البقرة - الدرس الأول",
-file: "bakara-1.mp3"
+file: "bakara-1.mp3",
+duration: "20 دقيقة"
 },
 
 {
 title: "تفسير سورة البقرة - الدرس الثاني",
-file: "bakara-2.mp3"
+file: "bakara-2.mp3",
+duration: "22 دقيقة"
 },
 
 {
 title: "تفسير سورة البقرة - الدرس الثالث",
-file: "bakara-3.mp3"
+file: "bakara-3.mp3",
+duration: "19 دقيقة"
 },
 
 {
 title: "تفسير سورة البقرة - الدرس الرابع",
-file: "bakara-4.mp3"
+file: "bakara-4.mp3",
+duration: "21 دقيقة"
 }
 
 ];
