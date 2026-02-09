01837abdeebf3198286aa22876a9f0c1c4fa38db
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-10 00:56:27 +0300

    Update azkardata.js

--- azkardata.js
+++ azkardata.js
@@ -1,15 +1,12 @@
-// azkardata.js - دروس سلسلة أنوار الأذكار
 const azkarLessons = [
     {
         title: "أنوار الأذكار - الدرس الأول",
         file: "anwar-alazkar-1.mp3.wav",
-        category: "الأذكار",
-        duration: "03:40"
+        category: "الأذكار"
     },
     {
         title: "أنوار الأذكار - الدرس الثاني",
         file: "anwar-alazkar-2.mp3.wav",
-        category: "الأذكار",
-        duration: "04:15" // يمكنك تعديل الوقت الفعلي للدرس الثاني
+        category: "الأذكار"
     }
 ];
