caed3bdc4e08089c3a32f408a54c7c4ffc6ac0f1
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-10 13:45:38 +0300

    Update hadith.data.js

--- hadith.data.js
+++ hadith.data.js
@@ -1,3 +1,8 @@
 const hadithLessons = [
-    { title: "شرح رياض الصالحين - الدرس 1", file: "reyad-alsalehen1.mp3", category: "حديث" }
+    {
+        title: "شرح رياض الصالحين - الدرس 1",
+        file: "reyad-alsalehen1.mp3",
+        category: "حديث"
+    }
+    // يمكنك إضافة دروس أخرى هنا بنفس الطريقة
 ];
