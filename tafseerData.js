7e3fe1522f4770bc59226a7506d5a8a8400dfb39
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-10 16:22:41 +0300

    Update tafseerData.js

--- tafseerData.js
+++ tafseerData.js
@@ -1,59 +1,47 @@
-const tafseerLessons = [
+const tafseerData = [
+    // دروس سورة الفاتحة
+    {
+        title: "تفسير سورة الفاتحة - الدرس الأول",
+        file: "fatiha-1.mp3",
+        surah: "الفاتحة",
+        cat: "تفسير"
+    },
+    {
+        title: "تفسير سورة الفاتحة - الدرس الثاني",
+        file: "fatiha-2.mp3",
+        surah: "الفاتحة",
+        cat: "تفسير"
+    },
 
-{
-title: "تفسير سورة الفاتحة - الدرس الأول",
-file: "fatiha-1.mp3",
-surah: "الفاتحة",
-lessonNumber: 1,
-duration: "15 دقيقة"
-},
-
-{
-title: "تفسير سورة الفاتحة - الدرس الثاني",
-file: "fatiha-2.mp3",
-surah: "الفاتحة",
-lessonNumber: 2,
-duration: "18 دقيقة"
-},
-
-{
-title: "تفسير سورة البقرة - الدرس الأول",
-file: "bakara-1.mp3",
-surah: "البقرة",
-lessonNumber: 1,
-duration: "20 دقيقة"
-},
-
-{
-title: "تفسير سورة البقرة - الدرس الثاني",
-file: "bakara-2.mp3",
-surah: "البقرة",
-lessonNumber: 2,
-duration: "22 دقيقة"
-},
-
-{
-title: "تفسير سورة البقرة - الدرس الثالث",
-file: "bakara-3.mp3",
-surah: "البقرة",
-lessonNumber: 3,
-duration: "19 دقيقة"
-},
-
-{
-title: "تفسير سورة البقرة - الدرس الرابع",
-file: "bakara-4.mp3",
-surah: "البقرة",
-lessonNumber: 4,
-duration: "21 دقيقة"
-},
-
-{
-title: "تفسير سورة البقرة - الدرس الخامس",
-file: "bakara-5.mp3",
-surah: "البقرة",
-lessonNumber: 5,
-duration: "19 دقيقة"
-}
-
+    // دروس سورة البقرة
+    {
+        title: "تفسير سورة البقرة - الدرس الأول",
+        file: "bakara-1.mp3",
+        surah: "البقرة",
+        cat: "تفسير"
+    },
+    {
+        title: "تفسير سورة البقرة - الدرس الثاني",
+        file: "bakara-2.mp3",
+        surah: "البقرة",
+        cat: "تفسير"
+    },
+    {
+        title: "تفسير سورة البقرة - الدرس الثالث",
+        file: "bakara-3.mp3",
+        surah: "البقرة",
+        cat: "تفسير"
+    },
+    {
+        title: "تفسير سورة البقرة - الدرس الرابع",
+        file: "bakara-4.mp3",
+        surah: "البقرة",
+        cat: "تفسير"
+    },
+    {
+        title: "تفسير سورة البقرة - الدرس الخامس",
+        file: "bakara-5.mp3",
+        surah: "البقرة",
+        cat: "تفسير"
+    }
 ];
