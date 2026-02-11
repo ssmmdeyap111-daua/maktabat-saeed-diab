1d0259eb19bddb654d71c8c51ecb0402b3b94172
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-11 21:54:35 +0300

    Update tawheed.data.js

--- tawheed.data.js
+++ tawheed.data.js
@@ -1,1 +1,8 @@
 
+const tawheedData = [
+    { title: "شرح كتاب التوحيد - الدرس 1", file: "tawheed-1.mp3" },
+    { title: "شرح كتاب التوحيد - الدرس 2", file: "tawheed-2.mp3" },
+    { title: "شرح كتاب التوحيد - الدرس 3", file: "tawheed-3.mp3" },
+    { title: "شرح كتاب التوحيد - الدرس 4", file: "tawheed-4.mp3" },
+    { title: "شرح كتاب التوحيد - الدرس 5", file: "tawheed-5.mp3" }
+];
