00b9ae1dddf2d9ad5b9c510dd652ec4ad4d429ed
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-12 21:36:13 +0300

    Update faraidData.js

--- faraidData.js
+++ faraidData.js
@@ -1,13 +1,13 @@
-const faraidLessons = [
-    { title: "شرح المنظومة الرحبية - الدرس 1", file: "al-rhapih1.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 2", file: "al-rhapih2.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 3", file: "al-rhapih3.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 4", file: "al-rhapih4.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 5", file: "al-rhapih5.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 6", file: "al-rhapih6.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 7", file: "al-rhapih7.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 8", file: "al-rhapih8.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 9", file: "al-rhapih9.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 10", file: "al-rhapih10.mp3", cat: "فرائض" },
-    { title: "شرح المنظومة الرحبية - الدرس 11", file: "al-rhapih11.mp3", cat: "فرائض" }
+const faraidData = [
+    { title: "شرح المنظومة الرحبية - الدرس 1", file: "al-rhapih1.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 2", file: "al-rhapih2.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 3", file: "al-rhapih3.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 4", file: "al-rhapih4.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 5", file: "al-rhapih5.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 6", file: "al-rhapih6.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 7", file: "al-rhapih7.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 8", file: "al-rhapih8.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 9", file: "al-rhapih9.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 10", file: "al-rhapih10.mp3" },
+    { title: "شرح المنظومة الرحبية - الدرس 11", file: "al-rhapih11.mp3" }
 ];
