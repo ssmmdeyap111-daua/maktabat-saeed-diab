76b869d5f6108f6c2086bdf3d0cb90e89c47e73f
موقع الشيخ سعيد مصطفى دياب <ssmmdeyap111@gmail.com>
2026-02-15 19:00:31 +0300

    Update tafseerData.js

--- tafseerData.js
+++ tafseerData.js
@@ -1,27 +1,95 @@
-// ملف بيانات دروس التفسير
-const tafseerData = [
-    const tafseerLessons = [
+const tafseerLessons = [
+    // --- سورة الفاتحة ---
     {
         surah: "الفاتحة",
         title: "تفسير سورة الفاتحة - الدرس 1",
-        file: "fatiha-1.mp3",
+        file: "https://archive.org/download/bakara-10_202602/fatiha-1.mp3",
         duration: "15 دقيقة"
     },
     {
         surah: "الفاتحة",
         title: "تفسير سورة الفاتحة - الدرس 2",
-        file: "fatiha-2.mp3",
+        file: "https://archive.org/download/bakara-10_202602/fatiha-2.mp3",
         duration: "18 دقيقة"
+    },
+
+    // --- سورة البقرة ---
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 1",
+        file: "https://archive.org/download/bakara-10_202602/bakara-1.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 2",
+        file: "https://archive.org/download/bakara-10_202602/bakara-2.mp3",
+        duration: "22 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 3",
+        file: "https://archive.org/download/bakara-10_202602/bakara-3.mp3",
+        duration: "19 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 4",
+        file: "https://archive.org/download/bakara-10_202602/bakara-4.mp3",
+        duration: "21 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 5",
+        file: "https://archive.org/download/bakara-10_202602/bakara-5.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 6",
+        file: "https://archive.org/download/bakara-10_202602/bakara-6.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 7",
+        file: "https://archive.org/download/bakara-10_202602/bakara-7.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 8",
+        file: "https://archive.org/download/bakara-10_202602/bakara-8.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 9",
+        file: "https://archive.org/download/bakara-10_202602/bakara-9.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 10",
+        file: "https://archive.org/download/bakara-10_202602/bakara-10.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 11",
+        file: "https://archive.org/download/bakara-10_202602/bakara-11.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 12",
+        file: "https://archive.org/download/bakara-10_202602/bakara-12.mp3",
+        duration: "20 دقيقة"
+    },
+    {
+        surah: "البقرة",
+        title: "تفسير سورة البقرة - الدرس 13",
+        file: "https://archive.org/download/bakara-10_202602/bakara-13.mp3",
+        duration: "20 دقيقة"
     }
-];
-    
-    { t: "تفسير سورة البقرة - الدرس 1", f: "tafseer-2.mp3" },
-    { t: "تفسير سورة البقرة - الدرس 2", f: "tafseer-3.mp3" },
-    { t: "تفسير سورة البقرة - الدرس 3", f: "tafseer-4.mp3" },
-    { t: "تفسير سورة البقرة - الدرس 4", f: "tafseer-5.mp3" },
-    { t: "تفسير سورة آل عمران - الدرس 1", f: "tafseer-6.mp3" },
-    { t: "تفسير سورة آل عمران - الدرس 2", f: "tafseer-7.mp3" },
-    { t: "تفسير سورة آل عمران - الدرس 3", f: "tafseer-8.mp3" },
-    { t: "تفسير سورة آل عمران - الدرس 4", f: "tafseer-9.mp3" },
-    { t: "تفسير سورة آل عمران - الدرس 5", f: "tafseer-10.mp3" }
 ];
